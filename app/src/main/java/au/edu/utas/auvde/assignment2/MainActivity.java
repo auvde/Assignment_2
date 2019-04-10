761cc09e172858e8b090bd32e1b021aef1ba36ea
auvde <auvde@utas.edu.au>
2019-04-10 10:47:56 +1000

    text area

--- app/src/main/java/au/edu/utas/auvde/assignment2/MainActivity.java
+++ app/src/main/java/au/edu/utas/auvde/assignment2/MainActivity.java
@@ -3,11 +3,13 @@ package au.edu.utas.auvde.assignment2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static au.edu.utas.auvde.assignment2.R.*;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
     }
 }
